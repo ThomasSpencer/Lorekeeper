492c2036b3bd0ebe4544acbcbac0932c8c917c64
Thomas Spencer <t.spencer1990@hotmail.com>
2016-10-25 16:26:06 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,102 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161025202314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.integer  "city_id"
+    t.string   "name"
+    t.string   "profession"
+    t.text     "backstory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.integer  "continent_id"
+    t.string   "name"
+    t.integer  "population"
+    t.text     "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "continents", force: :cascade do |t|
+    t.integer  "plane_id"
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "creatures", force: :cascade do |t|
+    t.integer  "landscape_id"
+    t.string   "name"
+    t.string   "type"
+    t.string   "size"
+    t.integer  "strength"
+    t.integer  "dexterity"
+    t.integer  "constitution"
+    t.integer  "intelligence"
+    t.integer  "wisdom"
+    t.integer  "charisma"
+    t.text     "abilities"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "landscapes", force: :cascade do |t|
+    t.integer  "continent_id"
+    t.string   "name"
+    t.string   "terrain"
+    t.text     "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "planes", force: :cascade do |t|
+    t.integer  "story_id"
+    t.string   "name"
+    t.string   "colour"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.integer  "story_id"
+    t.string   "character_name"
+    t.string   "player_name"
+    t.text     "backstory"
+    t.integer  "strength"
+    t.integer  "dexterity"
+    t.integer  "constitution"
+    t.integer  "intelligence"
+    t.integer  "widsom"
+    t.integer  "charisma"
+    t.integer  "expereince"
+    t.string   "alignment"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer  "story_id"
+    t.integer  "session_number"
+    t.text     "notes"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "total_players"
+    t.string   "ruleset"
+    t.integer  "total_sessions"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
