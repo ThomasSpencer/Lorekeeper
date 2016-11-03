f5cdd31aac461523169ebe99d5aaca6c4120b0d6
Thomas Spencer <t.spencer1990@hotmail.com>
2016-11-03 15:15:20 -0400

    Added landing & sessions controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'landing/index'
+  get 'landing/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'stories/index'
 
-  root 'stories#index'
+  root 'landing#index'
 end
