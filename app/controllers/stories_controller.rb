fd338ad8a829b6a2b8d38f87b7b524f9f6dc3678
Thomas Spencer <t.spencer1990@hotmail.com>
2016-11-04 17:03:31 -0400

    Added controller functionality to stories controller.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,31 +1,41 @@
 class StoriesController < ApplicationController
 
   def index
-
+    @stories = Story.all
   end
 
   def show
-
+    @story = Story.find(params[:id])
   end
 
   def new
-
+    @story = Story.new
   end
 
   def edit
-
+    @story = Story.find(params[:id])
   end
 
   def create
-
+    @story = Story.new(story_params)
+    @story.total_sessions = 0
+    @story.save
   end
 
   def update
-
+    @story = Story.find(params[:id])
+    @story.update(story_params)
   end
 
   def destroy
-
+    @story = Story.find(params[:id])
+    @story.destroy
   end
-  
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :total_players, :ruleset)
+  end
+
 end
