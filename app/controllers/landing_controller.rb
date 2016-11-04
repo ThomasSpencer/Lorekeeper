5ffeb0d8f640af372622fd0624effd9ec57377cc
Thomas Spencer <t.spencer1990@hotmail.com>
2016-11-04 16:04:35 -0400

    Added RESTful actions to all the controllers.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,31 @@
 class LandingController < ApplicationController
+  
   def index
+
   end
 
   def show
+
   end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,31 @@
 class StoriesController < ApplicationController
 
   def index
-    @stories = Story.all
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
   
 end
