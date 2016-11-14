344f3fab6584df8d24f5f7a0fa2c42a05cfff23c
Thomas Spencer <t.spencer1990@hotmail.com>
2016-11-14 15:20:47 -0500

    Modified nested routes to allow for passing parent id's through params.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :stories do
-    resources :characters, :cities, :continents, :creatures, :landscapes, :planes, :players, :sessions
+    resources :characters, :planes, :players, :sessions
+  end
+
+  resources :planes, only: [] do
+    resources :continents
+  end
+
+  resources :continents, only: [] do
+    resources :cities, :landscapes
+  end
+
+  resources :cities, only: [] do
+    resources :characters
+  end
+
+  resources :landscapes, only: [] do
+    resources :creatures
   end
 
   root 'landing#index'
