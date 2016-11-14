c7d5287c3f31218d1bd781c2846cc5d0c7f0fe00
Thomas Spencer <t.spencer1990@hotmail.com>
2016-11-14 15:29:20 -0500

    Added RESTful actions for continents controller. Also created simple continents#index view.

--- app/controllers/continents_controller.rb
+++ app/controllers/continents_controller.rb
@@ -1,29 +1,41 @@
 class ContinentsController < ApplicationController
-  def index
+  before_action :find_plane
 
+  def index
+    @continetns = Continent.all
   end
 
   def show
-
+    @continent = Continent.find(params[:id])
   end
 
   def new
-
+    @continent = Continent.new
   end
 
   def edit
-
+    @continent = Continent.find(params[:id])
   end
 
   def create
+    @continent = Continent.new
 
+    if @continent.save
+      redirect_to stories_continents_path
+    else
+      render 'new'
+    end
   end
 
   def update
-
+    @contient = Continent.find(params[:id])
+    @continent.update(continent_params)
   end
 
   def destroy
-
+    @continent = Continent.find(params[:id])
+    if @continent.destory
+      redirect_to stories_continents_path
+    end
   end
 end
