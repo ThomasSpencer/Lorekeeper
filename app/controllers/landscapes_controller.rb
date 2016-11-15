a1c3862377ca2c5509058de9bde743d5e7867915
Thomas Spencer <t.spencer1990@hotmail.com>
2016-11-15 15:36:51 -0500

    Added method functions for landscapes controller.

--- app/controllers/landscapes_controller.rb
+++ app/controllers/landscapes_controller.rb
@@ -1,30 +1,50 @@
 class LandscapesController < ApplicationController
-  def index
+  before_action :find_continent
 
+  def index
+    @landscapes = Landscape.all
   end
 
   def show
-
+    @landscape = Landscape.find(params[:id])
   end
 
   def new
-
+    @landscape = Landscape.new
   end
 
   def edit
-
+    @landscape = Landscape.find(params[:id])
   end
 
   def create
+    @landscape = Landscape.new(landscape_params)
 
+    if @landscape.save
+      redirect_to continent_landscapes_path(@continent)
+    else
+      render 'new'
+    end
   end
 
   def update
+    @landscape = Continent.find(params[:id])
 
+    if @landscape.update
+      redirect_to continent_landscape_path(@contient, @landscape)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @landscape = Continent.find(params[:id])
 
+    if @landscape.destroy
+      redirect_to continent_landscapes_path(@continent)
+    else
+      render 'show'
+    end
   end
 
 end
