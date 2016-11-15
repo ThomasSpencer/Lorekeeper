c03b4bf4d3660af2289d98080d829ecf10485e10
Thomas Spencer <t.spencer1990@hotmail.com>
2016-11-15 16:01:46 -0500

    Added method functionality to characters controller.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,30 +1,50 @@
 class CharactersController < ApplicationController
-  def index
+  before_action :find_city
 
+  def index
+    @characters = Character.all
   end
 
   def show
-
+    @character = Character.find(params[:id])
   end
 
   def new
-
+    @character = Character.new
   end
 
   def edit
-
+    @character = Character.find(params[:id])
   end
 
   def create
+    @character = Character.new(character_params)
+    @character.city_id = @city.id
 
+    if @character.save
+      redirect_to city_characters_path(@city)
+    else
+      render 'new'
+    end
   end
 
   def update
+    @character = Character.find(params[:id])
 
+    if @character.update(character_params)
+      redirect_to city_character_path(@city, @character)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @character = Character.find(params[:id])
 
+    if @character.destroy
+      redirect_to city_characters_path(@city)
+    else
+      render 'show'
   end
 
 end
